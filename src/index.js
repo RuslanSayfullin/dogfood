895c388eaad96f8ccf64e8fea68f9656aa9caeca
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-28 12:46:37 +0500

    Переделали под себя

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
 
 ReactDOM.render(
-	<App/>,
+	<div className="wrapper">
+		<div className="card">=)</div>
+		<div className="card">o_0</div>
+		<div className="card">^_^</div>
+		<div className="card">=(</div>
+	</div>,
 	document.querySelector("#root")
-);
+)
