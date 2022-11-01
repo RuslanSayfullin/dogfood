ed5e18aa0a41603cc968ec0adbaacd889a10d3d4
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-11-01 10:09:31 +0500

    Добавили компонент

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+class Card extends React.Component {
+	render() {
+		return(
+			<div className="card">
+				<div className="like">&lt;3</div>
+				^_^
+				<h6>Ня</h6>
+			</div>
+		)
+	}
+}
+
 
 ReactDOM.render(
 	<div className="wrapper">
-		<div className="card">=)</div>
+		{/* <div className="card">=)</div>
 		<div className="card">o_0</div>
 		<div className="card">^_^</div>
-		<div className="card">=(</div>
+		<div className="card">=(</div> */}
+		<Card/>
+		<Card/>
+		<Card/>
+		<Card/>
 	</div>,
 	document.querySelector("#root")
 )
