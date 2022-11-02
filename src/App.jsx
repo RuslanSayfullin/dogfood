81a60fc015dfe764cba820974f5cb4ffc7f6c3ad
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-11-02 09:21:29 +0500

    Сделали вместе с преподоателем по видео уроку.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./components/Card";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import data from "./assets/data.json";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <div className="cards-container">
-                    {/* <Card/> */}
-                    {data.map((d, i) => <Card 
-                        key={i}
-                        img={d.picture}
-                        text={d.name}
-                        price={d.price}
-                    />)}
-                </div>
+const App = () => {
+    const [goods, setGoods] = useState(data);
+
+    return (
+        <div className="wrapper">
+            <Header products={data} update={setGoods}/>
+            <div className="cards-container">
+                {/* <Card/> */}
+                {goods.map((d, i) => <Card 
+                    key={i}
+                    img={d.picture}
+                    text={d.name}
+                    price={d.price}
+                />)}
             </div>
-        )
-    }
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
--- src/intro/index.js
+++ src/intro/index.js
@@ -2,36 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// (1 + 2) / 3
+
 class Card extends React.Component {
 	render() {
-		return(
-			<div className="card">
-				<div className="like">&lt;3</div>
-				{this.props.smile}
-				<h6>{this.props.text}</h6>
-			</div>
-		)
+		return <div className={"card " + this.props.className}>
+			<div className="like">&lt;3</div>
+			{this.props.smile}
+			<h6>{this.props.text}</h6>
+		</div>
+		
 	}
 }
 
 const emotions = [
-	{smile: "=)", descriptions: "Ы." },
-	{smile: "0_o", descriptions: "Ooo..." },
-	{smile: "^_^", descriptions: "МММ ня" },
-	{smile: "=(", descriptions: "Уф" }
-]
+	{smile: "=)", description: "Ы."},
+	{smile: "O_o", description: "Ооо..."},
+	{smile: "^_^", description: "Ммм ня"},
+	{smile: "=(", description: "Уф"}
+];
 
 ReactDOM.render(
 	<div className="wrapper">
-		{/* <div className="card">=)</div>
-		<div className="card">o_0</div>
-		<div className="card">^_^</div>
-		<div className="card">=(</div> */}
-		{/* <Card smile="=)" text="Ы." />
-		<Card smile="0_o" text="Ooo..." />
-		<Card smile="^_^" text="МММ ня" />
-		<Card smile="=(" text="Уф" /> */}
-		{emotions.map(el => <Card smile={el.smile} text={el.description} key={i} />)}
+		{/* v1 */}
+		<div className="card">=)</div>	
+		<div className="card">O_o</div>	
+		<div className="card">^_^</div>	
+		<div className="card">=(</div>
+
+		{/* v2 */}
+		<Card smile="=)" text="Ы." className="red" />
+		<Card smile="O_o" text="Ооо..." />
+		<Card smile="^_^" text="Ммм ня" />
+		<Card smile="=(" text="Уф" />
+
+		{/* v3 */}
+		{emotions.map((el,i) => <Card smile={el.smile} text={el.description} key={i}/>)}
+		{/* <div/>
+		<img/> */}
 	</div>,
 	document.querySelector("#root")
 )
+
+/*
+	Метод map:
+		["Яблоко", "Арбуз", "Банан"] => ["<p>Яблоко</p>", "<p>Арбуз</p>", "<p>Банан</p>"]
+*/
